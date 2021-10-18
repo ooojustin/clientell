6bcee7760930399dffdda71e057b8c1e5bec7fe8
Justin <justin@garofolo.net>
2021-10-18 15:25:22 -0400

    basic user create endpoint

--- backend/controllers/user.go
+++ backend/controllers/user.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"rc.justin.ooo/config"
+	"rc.justin.ooo/models"
 )
 
 type UserController struct{}
@@ -11,11 +13,28 @@ type UserController struct{}
 func (u UserController) Setup(router *gin.Engine) {
 	userGroup := router.Group("user")
 	{
-		userGroup.POST("/create", u.CreateAccount)
+		userGroup.POST("/create", u.Create)
 	}
 }
 
-func (u UserController) CreateAccount(c *gin.Context) {
-	// TODO
-	c.String(http.StatusOK, "create account")
+func (u UserController) Create(c *gin.Context) {
+
+	// bind json posted data to form for creating accounts
+	ucf := &models.UserCreateForm{}
+	err := c.BindJSON(&ucf)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// create db user from form
+	user := new(models.User)
+	dbuser := user.FromUCF(ucf)
+
+	// create object in database
+	config.DB.Create(dbuser)
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+	})
+
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,18 +1,39 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	FirstName    string `json:"first_name" gorm:"size:128"`
-	LastName     string `json:"last_name" gorm:"size:128"`
-	Email        string `json:"email" gorm:"size:128,unique"`
-	PasswordHash []byte `json:"-"`
+	FirstName string `json:"first_name" gorm:"size:128"`
+	LastName  string `json:"last_name" gorm:"size:128"`
+	Email     string `json:"email" gorm:"size:128,unique"`
+	Password  string `json:"-"`
 }
 
-type UserSignupForm struct {
+type UserCreateForm struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
+}
+
+func (u User) FromUCF(ucf *UserCreateForm) *User {
+
+	// hash password user bcrypt
+	bpassword := []byte(ucf.Password)
+	bhash, _ := bcrypt.GenerateFromPassword(bpassword, bcrypt.DefaultCost)
+
+	// create gorm user model
+	user := &User{
+		FirstName: ucf.FirstName,
+		LastName:  ucf.LastName,
+		Email:     ucf.Email,
+		Password:  string(bhash),
+	}
+
+	return user
+
 }
