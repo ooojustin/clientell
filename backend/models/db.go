e08bfb29f5bb4b1a8dd02c21ea05cac87abeff7d
Justin <justin@garofolo.net>
2021-10-25 21:25:20 -0400

    replace development db with remote mysql alternative

--- backend/models/db.go
+++ backend/models/db.go
@@ -3,7 +3,6 @@ package models
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -13,13 +12,15 @@ func InitDatabase() {
 
 	// connect to database and panic if failed
 	// using remote mysql db in release and local mysqlite db in debug
-	var err error
+	var dsn string
 	if gin.Mode() == gin.ReleaseMode {
-		dsn := "alpha:e%2V672vKCrz6z@tcp(clientellapp.com:3306)/alpha?charset=utf8mb4&parseTime=True&loc=Local"
-		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		dsn = "alpha:e%2V672vKCrz6z@tcp(clientellapp.com:3306)/alpha?charset=utf8mb4&parseTime=True&loc=Local"
 	} else {
-		DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+		dsn = "dev:e%2V672vKCrz6z@tcp(clientellapp.com:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
 	}
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database.")
