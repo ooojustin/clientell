c7d40996f614948bd98ca7d0e7f4952b134c950a
Justin <justin@garofolo.net>
2021-10-30 13:52:46 -0400

    basic logout button for staff panel

--- panel/src/components/Navbar.vue
+++ panel/src/components/Navbar.vue
@@ -5,7 +5,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
@@ -14,6 +14,7 @@
                         <router-link class="nav-link" to="/moderate">Moderate</router-link>
                     </li>
                 </ul>
+                <button class="btn btn-md btn-danger" type="button" @click="doLogout">Logout</button>
             </div>
         </div>
     </nav>
@@ -21,7 +22,16 @@
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    methods: {
+        doLogout() {
+            localStorage.removeItem("token");
+            this.$toast.warning("You have been logged out.", { position: "top" });
+            setTimeout(() => { 
+                location.reload();
+            }, 1500);
+        }
+    }
 }
 </script>
 
